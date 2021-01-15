78df7fe8f528698fabc60872deced6515d3b3ba8
Sotatek-ThienNguyen2 <thien.nguyen2@sotatek.com>
2021-01-15 22:03:00 +0700

    feat: add test

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,20 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('responds with detail about the current users', async () => {
+	const cookie = await global.signin();
+	const response = await request(app)
+		.get('/api/users/currentuser')
+		.set('Cookie', cookie)
+		.send()
+		.expect(200)
+	expect(response.body.currentUser.email).toEqual('test@test.com')
+})
+
+it('responds with null if not authenticated', async ()=> {
+	const response = await request(app)
+			.get('/api/users/currentuser')
+			.send()
+			.expect(200)
+	expect(response.body.currentUser).toEqual(null)
+})
