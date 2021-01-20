22d152fff8462028b7ce4bacaecfeb3e8bfd81ef
thienNguyen2 <thien.nguyen2@sotatek.com>
2021-01-21 00:57:02 +0700

    feat: add client

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import {currentUser} from '../middlewares/current-user'
+import {currentUser} from '@tian-ticketing/common'
 
 const router = express.Router();
 
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import buildClient from '../api/build-client'
+import Header from '../components/header'
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
+      <Header currentUser={currentUser} />
         <Component {...pageProps} />
 
     </div>
